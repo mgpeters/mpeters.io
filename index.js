9f77f7315dd275b5da38d09f77ee100a802ce393
Matt Peters <matthew.george.peters@gmail.com>
2019-03-18 18:08:23 -0400

    imported nodemailer code via website

--- index.js
+++ index.js
@@ -14,7 +14,46 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/api/form', (req, res) => {
-    console.log(req.body);
+    nodemailer.createTestAccount((err, account) => { //https://nodemailer.com/about/
+        const htmlEmail = `
+            <h3>Contact Details</h3>
+            <ul>
+                <li>Name: ${req.body.name}</li>
+                <li>Email: ${req.body.email}</li>
+            </ul>
+            <h3>Message</h3>
+            <p>${req.body.message}</p>
+        `
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email", //Change to gmail
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: account.user, // generated ethereal user
+                pass: account.pass // generated ethereal password
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: "bar@example.com, baz@example.com",
+            replyTo: 'test@testaccount.com', // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: req.body.message, // plain text body
+            html: htmlEmail// html body
+        };
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err){
+                return console.log(err);
+            }
+            console.log('Message Sent: %s', info.message);
+            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info));
+        })
+    })
 })
 
 const PORT = process.env.PORT || 3001;
