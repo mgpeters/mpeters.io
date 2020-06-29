1035b57d747507b345ac073159a77ea9208089d0
Matt Peters <matthew.george.peters@gmail.com>
2020-06-29 18:21:13 -0400

    added config for dynamic links'

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  social: {
+    twitter: {
+      name: 'Twitter',
+      url: 'https://twitter.com/MattyPeters',
+      order: 4,
+    },
+    linkedIn: {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/mattgpeters/',
+      order: 2,
+    },
+    angelList: {
+      name: 'Angel List',
+      url: 'https://angel.co/u/mgpeters',
+      order: 3,
+    },
+    github: {
+      name: 'Github',
+      url: 'https://github.com/mgpeters/',
+      order: 1,
+    },
+  },
+};
