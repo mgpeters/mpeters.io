185e5bb18ec6ae8faf021ce4d36ceef731a02150
Matt Peters <matthew.george.peters@gmail.com>
2020-08-11 10:01:08 -0400

    setup basic webpack, modified file names, added pkg-lock to git ignore

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: {
+    src: './src//script/index.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          // sass-loader - Loads a Sass/SCSS file and compiles it to CSS.
+          // css-loader - Interprets @import and url() like import/require() and will resolve them.
+          // style-loader - Inject CSS into the DOM.
+          'style-loader', 'css-loader', 'sass-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    publicPath: '/build',
+  },
+};
