29a5c01f907afd4af4a1b23f7efd2d7365ea4c0b
Matt Peters <matthew.george.peters@gmail.com>
2020-09-20 15:39:17 -0400

    added eslint jest plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    jest: true,
   },
   extends: ['airbnb-base', 'prettier'],
   globals: {
@@ -13,7 +14,13 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
-  rules: {},
+  plugins: ['@typescript-eslint', 'eslint-plugin-jest'],
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
   root: true,
 };
--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,16 +1,18 @@
-test('generate links', () => {
-  document.body.innerHTML = `
+describe('index.ts tests', () => {
+  it('should generate social media links', () => {
+    document.body.innerHTML = `
     <section class="page-links">
       <div id="render-div" class="page-links__render"></div>
     </section>
   `;
-  const { orderArray, generateLinks } = require('../src/index');
-  const { socialLinks } = require('../src/config/config');
+    const { orderArray, generateLinks } = require('../src/index');
+    const { socialLinks } = require('../src/config/config');
 
-  const renderDiv = document.getElementById('render-div');
+    const renderDiv = document.getElementById('render-div');
 
-  generateLinks(orderArray(socialLinks));
+    generateLinks(orderArray(socialLinks));
 
-  console.log('html', document.body.innerHTML);
-  // expect()
+    // console.log('html', document.body.innerHTML);
+    // expect()
+  });
 });
