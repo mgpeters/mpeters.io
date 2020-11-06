6be6536d45232b2108625769390a8c7eba5f9095
Matt Peters <matthew.george.peters@gmail.com>
2020-11-06 10:44:19 -0500

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: ['@babel/preset-env'],
         },
       },
       {
